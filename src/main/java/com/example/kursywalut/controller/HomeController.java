50c82ce1e6ff8d348b4bd8ce8455d1eefdf2fbff
KasiaKoper <katarzynakoper2@gmail.com>
2019-06-02 15:58:56 +0200

    integration javascript with api added

---
+++ src/main/java/com/example/kursywalut/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.example.kursywalut.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+
+//z wykorzystaniem JavaScript
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home (ModelMap modelMap){
+        modelMap.put("currencies", Arrays.asList("eur","usd"));
+        return "home";
+    }
+
+
+}
