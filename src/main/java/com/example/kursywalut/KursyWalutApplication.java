59b93cd4bb7c0418a345a8fc84b2bc88c5b92813
KasiaKoper <katarzynakoper2@gmail.com>
2019-06-02 12:02:22 +0200

    first commit

---
+++ src/main/java/com/example/kursywalut/KursyWalutApplication.java
@@ -0,0 +1,14 @@
+package com.example.kursywalut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursyWalutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KursyWalutApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/com/example/kursywalut/KursyWalutApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.kursywalut;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KursyWalutApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
